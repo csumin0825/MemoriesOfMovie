00bb18870125ce3beffb20aca3279e4cf4696b87
sumin <csumin008255@gmail.com>
2022-11-27 16:26:58 +0900

    feat: tab 구현

--- app/src/main/java/com/example/memoriesofmovies/MainActivity.java
+++ app/src/main/java/com/example/memoriesofmovies/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.memoriesofmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Fragment1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Fragment2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Fragment3"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(TabLayout.Tab tab){
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab){
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab){
+            }
+
+
+        });
     }
 }
